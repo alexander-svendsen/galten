f2e0fdaca3b43f7fcbaf078a303b2aa079c41cff
Alexander Svendsen <alexandersvendsen@hotmail.com>
2016-01-02 02:40:08 +0100

    Use the correct port

--- server.js
+++ server.js
@@ -50,5 +50,5 @@ app.use('/weather', function(req, res) {
 app.get('/messages', getMessages);
 app.post('/messages', postMessage);
 
-app.listen(process.env.NODE_PORT);
-console.log("Server up and running on port " + process.env.NODE_PORT);
+app.listen(.listen(process.env.PORT || 8080));
+console.log("Server up and running on port " + .listen(process.env.PORT || 8080));
